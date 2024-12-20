e89befab223adf61ab0c3343746e653b02ced119
Abdullah <abdullah981066@gmail.com>
2024-12-20 19:16:09 +0530

    commit

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from 'react';
+
+const AnimatedTitle = ({ title, containerClass }) => {
+
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const ctx = gsap.context
+    }, [])
+
+  return (
+    <div
+    ref={containerRef}
+      className={`animated-title ${containerClass}`}
+    >
+      {title.split('<br />').map((line, index) => (
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(' ').map((word, i) => (
+            <span
+              key={i}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            ></span>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedTitle;
